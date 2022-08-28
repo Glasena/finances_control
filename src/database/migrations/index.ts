da6a3298a03ff34ce982d472e10cff8cb08c7e25
Alison Glasenapp <alisonglasenapp@gmail.com>
2022-08-28 17:41:09 -0300

    Initial Setup

---
+++ src/database/migrations/index.ts
@@ -0,0 +1,7 @@
+import CreateTransactions1661708046609 from './1661708046609-CreateTransactions';
+
+const migrations = {
+    CreateTransactions: CreateTransactions1661708046609
+}
+
+export default migrations;
